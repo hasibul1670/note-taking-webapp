0737471eca7755cbf42f4135b8deb056c55100c7
Md. Hasibul Islam <hasibulislam1670@gmail.com>
2024-11-11 21:43:01 +0600

    ⏪ changes :: Backend URL Changed

--- client/src/Providers/AuthProvider.jsx
+++ client/src/Providers/AuthProvider.jsx
@@ -7,8 +7,8 @@ import { app } from "../Components/firebase/firebase.config";
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
-// export const baseUrl = "https://server009.vercel.app/api/v1";
-export const baseUrl = "http://localhost:5000/api/v1";
+export const baseUrl = "https://server009.vercel.app/api/v1";
+// export const baseUrl = "http://localhost:5000/api/v1";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
